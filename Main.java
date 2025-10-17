311520a5657212733a27d653683ba304236e0fb6
dinathebanana <dqphung101@student.hbuhsd.edu>
2025-10-17 21:46:17 +0000

    hi

--- Main.java
+++ Main.java
@@ -4,6 +4,32 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    int N  0;
+    int sum = 0;
+    while (N > -1)
   }
 }
+
+public class Main
+{
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter numbers to add. ")
+
+    int N = 0;
+    int sum = 0;
+    int max = 0;
+    {
+      N = sc.nextInt();
+      sum += N;
+    }
+    if (sum.MAX_VALUE == true);
+    {
+      System.out.println("The largest score is " + max)
+    }
+      
+
+    }
+
+
